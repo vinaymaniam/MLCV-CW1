7747c9d8a0cda6040b2ee224b1f91cf1e3849dcc
vm1414 <vm1414@ic.ac.uk>
2018-02-20 18:11:20 +0000

    Q2 done

--- CW1/main_guideline.m
+++ CW1/main_guideline.m
@@ -1,4 +1,5 @@
 clear
+rng default
 % Simple Random Forest Toolbox for Matlab
 % written by Mang Shao and Tae-Kyun Kim, June 20, 2014.
 % updated by Tae-Kyun Kim, Feb 09, 2017
@@ -15,57 +16,61 @@ clear
 % Select dataset
 [data_train, data_test] = getData('Toy_Spiral'); % {'Toy_Gaussian', 'Toy_Spiral', 'Toy_Circle', 'Caltech'}
 
+% Set the random forest parameters
+N = 5;
 
-%%%%%%%%%%%%%
-% check the training and testing data
-    % data_train(:,1:2) : [num_data x dim] Training 2D vectors
-    % data_train(:,3) : [num_data x 1] Labels of training data, {1,2,3}
+nums = round(logspace(0, 2, N));
+depths = round(logspace(0.301, 1.114, N));
+splitnums = round(logspace(0, 1.3, N));
+
+% for i = 1:N
+    figure;  
+    tic
+    plot_toydata(data_train);
+
+    scatter(data_test(:,1),data_test(:,2),'.b');
     
-plot_toydata(data_train);
-
-    % data_test(:,1:2) : [num_data x dim] Testing 2D vectors, 2D points in the
-    % uniform dense grid within the range of [-1.5, 1.5]
-    % data_train(:,3) : N/A
     
-scatter(data_test(:,1),data_test(:,2),'.b');
+    param.num = 10;         % Number of trees
+    param.depth = 5;        % trees depth
+    param.splitNum = 3;     % Number of split functions to try
+    param.split = 'IG';     % Currently support 'information gain' only
+
+%     param.num = nums(i);         % Number of trees
+%     param.depth = depths(i);        % trees depth
+%     param.splitNum = splitnums(i);     % Number of split functions to try
+%     param.split = 'IG';     % Currently support 'information gain' only
+    %%%%%%%%%%%%%%%%%%%%%%
+    % Train Random Forest
+
+    % Grow all trees
+    trees = growTrees(data_train,param);
+    % trees = growTrees(data_train,param);
 
 
-% Set the random forest parameters for instance, 
-param.num = 10;         % Number of trees
-param.depth = 5;        % trees depth
-param.splitNum = 3;     % Number of split functions to try
-param.split = 'IG';     % Currently support 'information gain' only
+    %%%%%%%%%%%%%%%%%%%%%%
+    % Evaluate/Test Random Forest
 
-%%%%%%%%%%%%%%%%%%%%%%
-% Train Random Forest
-% rng default
-% % Q1
-% numbags = 4;
-% sz = round(0.6*size(data_train,1));
-% for i = 1:4
-% %     idx = round(1+ (sz-1)*rand(1, sz));
-% %     S(:,:,i) = data_train(idx,:);
-%     S(:,:,i) = datasample(data_train,sz);
+    % grab the few data points and evaluate them one by one by the leant RF
+    test_point = [-.5 -.7;...
+                   .4 .3;...
+                   -.7 .4;...
+                   .5 -.5];
+    for n=1:4
+        leaves = testTrees([test_point(n,:) 0],trees);
+        % average the class distributions of leaf nodes of all trees
+        p_rf = trees(1).prob(leaves,:);
+        p_rf_sum = sum(p_rf, 1)/length(trees);    
+        plot(test_point(n,1), test_point(n,2), 's', 'MarkerFaceColor', p_rf_sum, 'MarkerEdgeColor','k', 'MarkerSize', 10);
+        title(sprintf('Toy Spiral Test Results for %i SplitNum', nums(i)));
+    end
+    toc
 % end
-% Grow all trees
-trees = growTrees(data_train,param);
-% trees = growTrees(data_train,param);
-
-
-%%%%%%%%%%%%%%%%%%%%%%
-% Evaluate/Test Random Forest
-
-% grab the few data points and evaluate them one by one by the leant RF
-test_point = [-.5 -.7; .4 .3; -.7 .4; .5 -.5];
-for n=1:4
-    leaves = testTrees([test_point(n,:) 0],trees);
-    % average the class distributions of leaf nodes of all trees
-    p_rf = trees(1).prob(leaves,:);
-    p_rf_sum = sum(p_rf)/length(trees);
-end
 
 
 % Test on the dense 2D grid data, and visualise the results ... 
+
+
 
 % Change the RF parameter values and evaluate ... 
 
